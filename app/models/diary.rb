151ba02df64ba0330e654fe39f55b383c66ede3c
miyaseinto <cbj2877@yahoo.co.jp>
2020-09-28 10:27:25 +0900

    変更

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,18 @@
 class Diary < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do |assoc|
+    assoc.validates :taskbox1
+    assoc.validates :taskbox2
+    assoc.validates :taskbox3
+    assoc.validates :taskbox4
+    assoc.validates :taskbox5
+    assoc.validates :taskbox6
+    assoc.validates :taskbox7
+    assoc.validates :taskbox8
+    assoc.validates :taskbox9
+    assoc.validates :taskbox10
+    assoc.validates :diary
+    assoc.validates :date
+  end
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,5 @@
 class Target < ApplicationRecord
   belongs_to :user
+
+  validates :target, presence:true
 end
